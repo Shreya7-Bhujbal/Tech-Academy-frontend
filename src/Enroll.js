0b815aaaaad1edfe7489f1b3001735d2fe7f6d27
Shreya Bhujbal <shreya24cs@student.mes.ac.in>
2026-04-27 18:17:54 +0530

    Create Enroll.js

---
+++ src/Enroll.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Enroll() {
+  return (
+    <div>
+      <h1>Enroll Page</h1>
+      <p>Enroll in your favorite course.</p>
+    </div>
+  );
+}
+
+export default Enroll;
