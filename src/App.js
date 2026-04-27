91744583bb55727e8201e9a14296d39c466849f3
Shreya Bhujbal <shreya24cs@student.mes.ac.in>
2026-04-27 18:12:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import CourseDetail from "./CourseDetail";
+import MyEnrollments from "./MyEnrollments";
+import Enroll from "./Enroll";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/course" element={<CourseDetail />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/enroll" element={<Enroll />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
