abfb797c39f9fa482d2e1d9facc14942625ed298
Shreya Bhujbal <shreya24cs@student.mes.ac.in>
2026-04-27 18:13:36 +0530

    Add About component for web development platform

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This platform helps you learn modern web development.</p>
+    </div>
+  );
+}
+
+export default About;
