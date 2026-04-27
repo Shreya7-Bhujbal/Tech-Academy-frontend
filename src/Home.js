81d3b43d26b17d4e4d973c88702fb68bc2a19b07
Shreya Bhujbal <shreya24cs@student.mes.ac.in>
2026-04-27 18:13:14 +0530

    Add Home component with welcome message

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Tech Academy 🚀</h1>
+      <p>Learn MERN Stack, APIs, and more.</p>
+    </div>
+  );
+}
+
+export default Home;
